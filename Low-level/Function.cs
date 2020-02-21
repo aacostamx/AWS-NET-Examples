97592b6d55dce550448ab1325716facbef313e00
aacostamx <eltony162@gmail.com>
2020-02-21 11:05:10 -0600

    low leve add into DynamoDB

--- Low-level/Function.cs
+++ Low-level/Function.cs
@@ -20,7 +20,8 @@ namespace Low_level
                 {
                     int orderId = new Random().Next(1000000);
                     string customerId = Guid.NewGuid().ToString();
-                    var request = new PutItemRequest
+
+                    await client.PutItemAsync(new PutItemRequest
                     {
                         TableName = "Orders",
                         Item = new Dictionary<string, AttributeValue>
@@ -31,25 +32,23 @@ namespace Low_level
                             { "Date", new AttributeValue { S = DateTime.UtcNow.ToString("yyyy-MM-ddTHH:mm:ssZ") }},
                             { "Status", new AttributeValue { S = Convert.ToInt32(order.Status).ToString() }},
                         }
-                    };
-
-                    await client.PutItemAsync(request);
+                    });
 
                     Dictionary<string, AttributeValue> item = client.GetItemAsync(new GetItemRequest
                     {
                         TableName = "Orders",
                         Key = new Dictionary<string, AttributeValue>
-                    {
-                        { "OrderId", new AttributeValue { N = orderId.ToString() } },
-                        { "OrderId", new AttributeValue { N = customerId } }
-                    }
+                        {
+                            { "OrderId", new AttributeValue { N = orderId.ToString() } },
+                            { "CustomerId", new AttributeValue { S = customerId } }
+                        }
                     }).Result.Item;
 
-                    order.OrderId = int.Parse(item["OrderId"].S);
+                    order.OrderId = int.Parse(item["OrderId"].N);
                     order.CustomerId = item["CustomerId"].S;
                     order.ShipMethod = item["ShipMethod"].S;
                     order.Date = DateTime.Parse(item["Date"].S);
-                    order.Status = bool.Parse(item["Status"].N);
+                    order.Status = Convert.ToBoolean(int.Parse(item["Status"].S));
                 }
             }
             catch (Exception ex)
@@ -58,5 +57,6 @@ namespace Low_level
             }
             return order;
         }
+
     }
 }

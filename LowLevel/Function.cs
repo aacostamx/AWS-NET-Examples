a7fb99ebfcf9ab1700c3f404297a4e755d400725
aacostamx <eltony162@gmail.com>
2020-02-21 12:01:24 -0600

    rename input variable

--- LowLevel/Function.cs
+++ LowLevel/Function.cs
@@ -13,7 +13,7 @@ namespace LowLevel
 {
     public class Function
     {
-        public async Task<Order> FunctionHandler(Order order, ILambdaContext context)
+        public async Task<Order> FunctionHandler(Order input, ILambdaContext context)
         {
             try
             {
@@ -29,13 +29,13 @@ namespace LowLevel
                         {
                             { "OrderId", new AttributeValue { N = orderId.ToString()}},
                             { "CustomerId", new AttributeValue { S =  customerId}},
-                            { "ShipMethod", new AttributeValue { S = order.ShipMethod }},
+                            { "ShipMethod", new AttributeValue { S = input.ShipMethod }},
                             { "Date", new AttributeValue { S = DateTime.UtcNow.ToString("yyyy-MM-ddTHH:mm:ssZ") }},
-                            { "Status", new AttributeValue { S = Convert.ToInt32(order.Status).ToString() }},
+                            { "Status", new AttributeValue { S = Convert.ToInt32(input.Status).ToString() }},
                         }
                     });
 
-                    Dictionary<string, AttributeValue> item = client.GetItemAsync(new GetItemRequest
+                    Dictionary<string, AttributeValue> order = client.GetItemAsync(new GetItemRequest
                     {
                         TableName = "Orders",
                         Key = new Dictionary<string, AttributeValue>
@@ -45,19 +45,18 @@ namespace LowLevel
                         }
                     }).Result.Item;
 
-                    order.OrderId = int.Parse(item["OrderId"].N);
-                    order.CustomerId = item["CustomerId"].S;
-                    order.ShipMethod = item["ShipMethod"].S;
-                    order.Date = DateTime.Parse(item["Date"].S);
-                    order.Status = Convert.ToBoolean(int.Parse(item["Status"].S));
+                    input.OrderId = int.Parse(order["OrderId"].N);
+                    input.CustomerId = order["CustomerId"].S;
+                    input.ShipMethod = order["ShipMethod"].S;
+                    input.Date = DateTime.Parse(order["Date"].S);
+                    input.Status = Convert.ToBoolean(int.Parse(order["Status"].S));
                 }
             }
             catch (Exception ex)
             {
                 context.Logger.Log(ex.ToString());
             }
-            return order;
+            return input;
         }
-
     }
 }
